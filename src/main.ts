2e91f473e6261102048f995dec7b4e7102c5d390
Yuta Mori <moriyu54321@gmail.com>
2022-12-14 12:42:16 +0900

    copy-markdown-link-github-issue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 import "./style.css";
 
-function copy({ plain }: { plain: string }) {
-  const listener = (event: any) => {
-    event.clipboardData.setData("text/plain", plain);
-    event.preventDefault();
-  };
-  document.addEventListener("copy", listener);
-  document.execCommand("copy");
-  document.removeEventListener("copy", listener);
+function main() {
+  function copy({ plain }: { plain: string }) {
+    const listener = (event: any) => {
+      event.clipboardData.setData("text/plain", plain);
+      event.preventDefault();
+    };
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+  }
+
+  const url = document.URL;
+  const title = document.querySelector("h1.gh-header-title")?.textContent;
+
+  if (title == null) {
+    window.alert("Copy failed");
+    return;
+  }
+
+  const text = title
+    .split(" ")
+    .map((t) => t.trim())
+    .filter((t) => t !== "")
+    .join(" ");
+
+  copy({
+    plain: `[${text}](${url})`,
+  });
 }
 
 chrome.tabs.query(
@@ -16,9 +36,12 @@ chrome.tabs.query(
     currentWindow: true,
   },
   (tabs) => {
-    const { title, url } = tabs[0];
-    copy({
-      plain: `[${title}](${url})`,
-    });
+    const { id, url } = tabs[0];
+    if (id && url) {
+      chrome.scripting.executeScript({
+        target: { tabId: id },
+        func: main,
+      });
+    }
   }
 );
